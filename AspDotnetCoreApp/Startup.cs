146937c227eeb82479242550c42d85c0fce4adfa
Saket Vats <saketvatslpu@gmail.com>
2020-08-30 10:16:22 +0530

    Basic Checkin

--- AspDotnetCoreApp/Startup.cs
+++ AspDotnetCoreApp/Startup.cs
@@ -27,7 +27,7 @@ namespace AspDotnetCoreApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!"+"<br/>----------- Worker Process Name : "+ System.Diagnostics.Process.GetCurrentProcess().ProcessName);
             });
         }
     }
