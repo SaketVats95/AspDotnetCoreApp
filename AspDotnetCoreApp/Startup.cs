c00e5ec7fc5370b1aa3d4442496b42c0efc85cf6
Saket Vats <saketvatslpu@gmail.com>
2020-08-30 10:31:40 +0530

    Added config variable in appSettings.json file

--- AspDotnetCoreApp/Startup.cs
+++ AspDotnetCoreApp/Startup.cs
@@ -5,12 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspDotnetCoreApp
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +34,7 @@ namespace AspDotnetCoreApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!"+"<br/>----------- Worker Process Name : "+ System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                await context.Response.WriteAsync("myKey="+_config["MyKey"]+"<br/>----------- Worker Process Name : "+ System.Diagnostics.Process.GetCurrentProcess().ProcessName);
             });
         }
     }
